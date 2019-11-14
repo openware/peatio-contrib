87ec38019f7199399831721d8613b3080796408e
MoD <mod@websys.io>
2019-11-14 17:58:32 +0200

    Release with peatio-core 0.6.3

--- peatio-bitcoincash/lib/peatio/bitcoincash/version.rb
+++ peatio-bitcoincash/lib/peatio/bitcoincash/version.rb
@@ -1,5 +1,5 @@
 module Peatio
   module Bitcoincash
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
   end
 end
--- peatio-dash/lib/peatio/dash/version.rb
+++ peatio-dash/lib/peatio/dash/version.rb
@@ -2,6 +2,6 @@
 
 module Peatio
   module Dash
-    VERSION = "0.1.1"
+    VERSION = "0.1.2"
   end
 end
