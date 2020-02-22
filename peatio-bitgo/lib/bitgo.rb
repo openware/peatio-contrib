cda8ae7a77d994a409df80e6da6ba57368166224
Maksym Naichuk <mnaichuk@heliostech.fr>
2020-02-22 11:02:05 +0200

    Bitgo Plugin init

---
+++ peatio-bitgo/lib/bitgo.rb
@@ -0,0 +1,16 @@
+require "peatio"
+
+module Peatio
+  module Bitgo
+    require "bigdecimal"
+    require "bigdecimal/util"
+
+    require "peatio/bitgo/blockchain"
+    require "peatio/bitgo/client"
+    require "peatio/bitgo/wallet"
+
+    require "peatio/bitgo/hooks"
+
+    require "peatio/bitgo/version"
+  end
+end
---
+++ peatio-bitgo/spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require "bundler/setup"
+require "peatio/bitgo"
+require "pry-byebug"
+require "webmock/rspec"
+require "mocha"
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.mock_with :mocha
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
